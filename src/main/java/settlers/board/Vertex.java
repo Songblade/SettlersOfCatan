ba88449ec9023f4f3bfa7499032b345dfa1b581a
Songblade <sgreenga@mail.yu.edu>
2022-04-11 17:09:51 -0400

    Added methods to the Vertex interface

--- src/main/java/settlers/board/Vertex.java
+++ src/main/java/settlers/board/Vertex.java
@@ -14,6 +14,7 @@ public interface Vertex {
      *
      * @param vertex being set adjacent to the Vertex
      * @param position from 0 to 3, where the vertex is set, where 0 is the upper left, increasing clockwise
+     * @throws IllegalArgumentException if this position's vertex is already set
      */
     void setVertices(Vertex vertex, int position);
 
@@ -28,13 +29,33 @@ public interface Vertex {
      *
      * @param edge the adjacent edge being added
      * @param position from 0 to 3, where the edge is set, where 0 is the upper left, increasing clockwise
+     * @throws IllegalArgumentException if this position's edge is already set
      */
     void setEdge(Edge edge, int position);
 
     /**
-     * @param player that owns this vertex
-     *
+     * The player defaults as null until it is changed
+     * @return player that owns this Settlement or City, or null if there is no Settlement or City
+     */
+    Player getPlayer();
+
+    /**
+     * @param player that owns this Settlement or City
+     * @throws IllegalArgumentException if a different player already has this city
      */
     void setPlayer(Player player);
+
+    /**
+     *
+     * @return true if this is a City, false otherwise
+     */
+    boolean isCity();
+
+    /**
+     * Turns this settlement into a city
+     * @throws IllegalArgumentException if this is already a City
+     * @throws IllegalStateException if this is not a Settlement
+     */
+    void makeCity();
 
 }
