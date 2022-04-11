4f7f8640b54a79e9d860b73b1147d85828b236da
Songblade <sgreenga@mail.yu.edu>
2022-04-11 17:02:57 -0400

    Continued working on JavaDocs

--- src/main/java/settlers/board/Vertex.java
+++ src/main/java/settlers/board/Vertex.java
@@ -1,13 +1,40 @@
 package settlers.board;
 
+import settlers.Player;
+
 public interface Vertex {
 
-    // the vertices next to this one
-    public Vertex[] getAdjacentVertices();
+    /**
+     *
+     * @return a length 3 array containing the adjacent vertices, where 0 is up, increasing clockwise
+     */
+    Vertex[] getAdjacentVertices();
 
-    public void setVertices(Vertex vertex, int position);
+    /**
+     *
+     * @param vertex being set adjacent to the Vertex
+     * @param position from 0 to 3, where the vertex is set, where 0 is the upper left, increasing clockwise
+     */
+    void setVertices(Vertex vertex, int position);
 
-    // the edges next to this one
-    public Edge[] getEdges();
+    /**
+     *
+     * @return a length 3 array containing the adjacent edges, where 0 is up, increasing clockwise
+     * Each edge at a position connects to the vertex of the same number
+     */
+    Edge[] getEdges();
+
+    /**
+     *
+     * @param edge the adjacent edge being added
+     * @param position from 0 to 3, where the edge is set, where 0 is the upper left, increasing clockwise
+     */
+    void setEdge(Edge edge, int position);
+
+    /**
+     * @param player that owns this vertex
+     *
+     */
+    void setPlayer(Player player);
 
 }
