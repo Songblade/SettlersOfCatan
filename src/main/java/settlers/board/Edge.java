7da03506f2116e76b5d0e41ed8b642a5aa6e6b82
Songblade <sgreenga@mail.yu.edu>
2022-04-11 17:23:51 -0400

    Added Edge's methods

--- src/main/java/settlers/board/Edge.java
+++ src/main/java/settlers/board/Edge.java
@@ -1,4 +1,19 @@
 package settlers.board;
 
+import settlers.Player;
+
 public interface Edge {
+
+    /**
+     *
+     * @return the player who owns the road, or null if there is no road
+     */
+    Player getPlayer();
+
+    /**
+     * @param player that owns this Road
+     * @throws IllegalArgumentException if a different player already has this road
+     */
+    void setPlayer(Player player);
+
 }
