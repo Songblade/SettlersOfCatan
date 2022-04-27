daca295e3050ad8a8c8e70f5017831bb879b87de
Songblade <sgreenga@mail.yu.edu>
2022-04-27 17:17:41 -0400

    Added and tested number methods

--- src/main/java/settlers/board/HexImpl.java
+++ src/main/java/settlers/board/HexImpl.java
@@ -3,28 +3,45 @@ package settlers.board;
 import settlers.card.Resource;
 
 public class HexImpl implements Hex{
-    private Resource resource;
-    private int number = -1;
+    private final Resource resource;
+    private int number;
 
 
     public HexImpl(Resource resource){
+        if (resource == null) {
+            throw new IllegalArgumentException("Constructor is null");
+        }
         this.resource = resource;
+        number = -1; // the default value, before it is set
     }
 
     /**
-     *
-     * @param number the number we wish to set this object's number to
-     * Sets the hex's number to number. Should only be called once
+     * Sets the hex's number to number
+     * @param number we wish to set this object's number to
+     * @throws IllegalStateException if the hex already has a number
+     * @throws IllegalArgumentException if the number is out of bounds
      */
     public void setNumber(int number){
+        if (this.number != -1) {
+            throw new IllegalStateException("hex's number is already set to " + this.number);
+        }
+        if (number < 1 || number > 12 || number == 7) {
+            throw new IllegalArgumentException(number + " is out of bounds, must be between 1 and 12 excluding 7");
+        }
+        if (resource == Resource.MISC && number != 1) {
+            throw new IllegalStateException("desert must be set to 1");
+        }
         this.number = number;
     }
 
     /**
-     *
-     * @return the Hex's die number, equals to 7 if this is the desert
+     * @return the Hex's die number, equals to 1 if this is the desert
+     * @throws IllegalStateException if number hasn't been set yet
      */
     public int getNumber(){
+        if (number == -1) {
+            throw new IllegalStateException("number has not been set yet");
+        }
         return number;
     }
 
--- src/test/java/settlers/board/HexTest.java
+++ src/test/java/settlers/board/HexTest.java
@@ -28,4 +28,42 @@ public class HexTest {
         assertThrows(IllegalArgumentException.class, () -> new HexImpl(null));
     }
 
+    // Tests for set and get number
+    // makes sure get number throws when not set yet
+    @Test
+    public void getNumberThrowsUnset() {
+        Hex hex = new HexImpl(Resource.WOOD);
+        assertThrows(IllegalStateException.class, hex::getNumber);
+    }
+
+    // makes sure set number throws when number is out of bounds
+    @Test
+    public void setNumberThrowsOutOfBounds() {
+        Hex hex = new HexImpl(Resource.WOOD);
+        assertThrows(IllegalArgumentException.class, () -> hex.setNumber(7));
+        assertThrows(IllegalArgumentException.class, () -> hex.setNumber(0));
+        assertThrows(IllegalArgumentException.class, () -> hex.setNumber(-1));
+        assertThrows(IllegalArgumentException.class, () -> hex.setNumber(13));
+    }
+
+    // makes sure get number works when sets to something, including 7
+    @Test
+    public void setNumberWorks() {
+        for (int i = 1; i < 12; i++) {
+            if (i != 7) {
+                Hex hex = new HexImpl(Resource.WOOD);
+                hex.setNumber(i);
+                assertEquals(i, hex.getNumber());
+            }
+        }
+    }
+
+    // makes sure setNumber throws when number already set
+    @Test
+    public void setNumberThrowsAlreadySet() {
+        Hex hex = new HexImpl(Resource.WOOD);
+        hex.setNumber(3);
+        assertThrows(IllegalStateException.class, () -> hex.setNumber(4));
+    }
+
 }
