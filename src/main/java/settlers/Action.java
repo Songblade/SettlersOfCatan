ec56e49a74e6cf64a67ac35ebdb53fac6951127f
Solomon <sfirfer@mail.yu.edu>
2022-05-08 21:52:20 -0400

    made a few changes

--- src/main/java/settlers/Action.java
+++ src/main/java/settlers/Action.java
@@ -1,8 +1,28 @@
 package settlers;
 
+import settlers.board.Hex;
+import settlers.board.Vertex;
+import settlers.board.Edge;
+
 /**
  * An action type which can be preformed by players
  */
-public enum Action {
-    ROAD,SETTLEMENT,CITY,PASS
+public class Action {
+
+    public enum ActionType{
+        PASS,ROAD,CITY,SETTLEMENT,THIEF
+    }
+
+    public Hex hex;
+    public Vertex vertex;
+    public Edge road;
+    public Player otherPlayer;
+    public final Player player;
+
+    public final ActionType type;
+
+    public Action(Player player, ActionType type){
+        this.player = player;
+        this.type = type;
+    }
 }
